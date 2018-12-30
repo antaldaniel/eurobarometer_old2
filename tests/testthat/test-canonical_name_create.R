042e13e447f1a43c898f9e8887f7ed37aa3983c2
antaldaniel <antaldaniel@gmail.com>
2018-12-30 20:06:55 +0100

    corrected test

--- tests/testthat/test-canonical_name_create.R
+++ tests/testthat/test-canonical_name_create.R
@@ -2,10 +2,9 @@ library (eurobarometer)
 library (testthat)
 context("Creating canonical var names")
 
-canonical_name_create ( c("UPPER CASE VAR", "VAR NAME WITH % SYMBOL") )
 
 test_that("correct text changes", {
-  expect_equal(code_nuts1  ( c("UPPER CASE VAR", "VAR NAME WITH % SYMBOL") ),
+  expect_equal(canonical_name_create ( c("UPPER CASE VAR", "VAR NAME WITH % SYMBOL") ),
   c("upper_case_var", "var_name_with_pct_symbol")
   )
 })
